17ba9dddaf915d234671423b56d7817170e09b7d
Jason Wuerch <wuerchjason@gmail.com>
2016-01-22 09:40:28 -0800

    add in missing semicolons

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@ var pingPong = function(num) {
   console.log(num);
 
   if (!num.toString().match(/^[0-9]*$|^\s+/gmi)) {
-    return false
+    return false;
   } else {
     if (num % 15 === 0) {
-      return "pingpong"
+      return "pingpong";
     } else if (num % 5 === 0) {
-      return "pong"
+      return "pong";
     } else if (num % 3 === 0) {
-      return "ping"
-    }
-  }
-}
+      return "ping";
+    };
+  };
+};
