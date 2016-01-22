79c9a5323b575625f36d0686cffc0148d108f3c6
Jason Wuerch <wuerchjason@gmail.com>
2016-01-22 12:01:23 -0800

    realize that need to list all items up to a certain number in Ping Pong Game; make numbers appear as list in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,33 @@
 var pingPong = function(num) {
-  if (!num.toString().match(/^[0-9]*$|^\s+/gmi) || (num === 0) ) {
-    return "Please enter an integer.";
-  } else {
-    if (num % 15 === 0) {
-      return "pingpong";
-    } else if (num % 5 === 0) {
-      return "pong";
-    } else if (num % 3 === 0) {
-      return "ping";
+    var arr = []
+    if (!num.toString().match(/^[0-9]*$|^\s+/gmi) || (num === 0) ) {
+      return "Please enter an integer.";
     } else {
-      return num;
+      for (num; num > 0; num--) {
+        if (num % 15 === 0) {
+          arr.push("pingpong");
+        } else if (num % 5 === 0) {
+          arr.push("pong");
+        } else if (num % 3 === 0) {
+          arr.push("ping");
+        } else {
+          arr.push(num);
+        };
+      };
+      console.log(arr.reverse());
     };
-  };
 };
 
 $(document).ready(function() {
   $("#answerForm").submit(function(event) {
     var num = $("#numberInput").val();
     var result = pingPong(num)
-    $("ul").empty().text(result);
+    for (var i = 0; i <= num; i++) {
+      $("#listBox").append(result);
+    }
+
+
+
 
     (event).preventDefault();
   });
