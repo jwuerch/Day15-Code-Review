baa70d8969ff5931fb310a92ca158df033f62a79
Jason Wuerch <wuerchjason@gmail.com>
2016-01-22 12:13:30 -0800

    change 'if not a number divisible by either 5 or 3 return number' spec to a spec that returns numbers in an array

--- spec/specs.js
+++ spec/specs.js
@@ -3,6 +3,10 @@ describe("pingPong", function() {
     expect(pingPong(1.2)).to.equal("Please enter an integer.");
   });
 
+  it("returns number if not meeting any of the previous spec conditions, not divisible by 5 or 3", function() {
+    expect(pingPong(1)).to.eql([1]);
+  });
+
   it("checks to see if a number is divisible by just 3", function() {
     expect(pingPong(3)).to.eql([ 1, 2, 'ping' ]);
   });
@@ -11,13 +15,9 @@ describe("pingPong", function() {
     expect(pingPong(5)).to.eql([ 1, 2, 'ping', 4, 'pong' ]);
   });
 
-
   it("checks to see if a number is divisible by both 5 and 3", function() {
     expect(pingPong(15)).to.equal([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14,"pingpong"]);
   });
 
-  it("returns number if not meeting any of the previous spec conditions, not divisible by 5 or 3", function() {
-    expect(pingPong(16)).to.equal(16);
-  });
 
 });
